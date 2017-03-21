bd66e4779868183c3d6828a3fd19b1ef78eae716
rkdls <rkdls9@naver.com>
2017-03-21 12:27:33 +0900

    cnn튜토리얼 시작

--- youtube_tensorflow_lecture/chanwoolee/02cnn/CNNTUTORIAL.py
+++ youtube_tensorflow_lecture/chanwoolee/02cnn/CNNTUTORIAL.py
@@ -16,6 +16,9 @@ key, value = reader.read(filename_queue)
 x = tf.placeholder(tf.float32, shape=[None, IMAGE_WIDTH, IMAGE_HEIGHT])
 y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
+image_decoded = tf.image.decode_png(value)
+x = tf.cast(image_decoded, tf.float32)
+
 W_hidden1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32]))
 B_hidden1 = tf.Variable(tf.zeros(32))
 
@@ -23,18 +26,21 @@ x_image = tf.reshape(x, [-1, IMAGE_WIDTH, IMAGE_HEIGHT, 1])
 conv1 = tf.nn.conv2d(x_image, W_hidden1, strides=[1, 1, 1, 1], padding='SAME')
 hidden1 = tf.nn.relu(conv1 + B_hidden1)
 
-W_hidden2 = tf.Variable(tf.truncated_normal([5,5,32,64]))  #64개쓸거다.
+W_hidden2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64]))  # 64개쓸거다.
 B_hidden2 = tf.Variable(tf.truncated_normal([64]))
 
-conv2 = tf.nn.conv2d(hidden1, W_hidden2, strides=[1,1,1,1], padding='SAME')
-hidden2 = tf.nn.relu(conv2)
+conv2 = tf.nn.conv2d(hidden1, W_hidden2, strides=[1, 1, 1, 1], padding='SAME')
+hidden2 = tf.nn.relu(conv2 + B_hidden2)
+h_flat = tf.reshape(hidden2, [-1, 49 * 61 * 64])
+fc_w = tf.Variable(tf.truncated_normal([49 * 61 * 64, 1]))
+fc_b = tf.Variable(tf.zeros([1]))
 
-image_decoded = tf.image.decode_png(value)
-
+fc = tf.nn.relu(tf.matmul(h_flat, fc_w) + fc_b)
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    img = sess.run(image_decoded)
+    sess.run(tf.global_variables_initializer())
+    img = sess.run(fc)
     print(img)
 
     coord.request_stop()
