87345f289247cab1551eedeb0d6d67f1e2c12cb1
rkdls <rkdls9@naver.com>
2017-04-18 20:17:19 +0900

    DYNAMIC RNN예제

--- hands-on-machinelearning/RNNExample.py
+++ hands-on-machinelearning/RNNExample.py
@@ -1,33 +1,30 @@
 import tensorflow as tf
+import numpy as np
 
 n_inputs = 3
 n_neurons = 5
+n_steps = 2
 
-X0 = tf.placeholder(tf.float32, [None, n_inputs])
-X1 = tf.placeholder(tf.float32, [None, n_inputs])
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+X_seqs = tf.unstack(tf.transpose(X, perm=[1, 0, 2]))
 
-# Wx = tf.Variable(tf.random_normal(shape=[n_inputs, n_neurons], dtype=tf.float32))
-# Wy = tf.Variable(tf.random_normal(shape=[n_neurons, n_neurons], dtype=tf.float32))
-# b = tf.Variable(tf.zeros([1, n_neurons], dtype=tf.float32))
-#
-# Y0 = tf.tanh(tf.matmul(X0, Wx) + b) # X0:(?,3) Wx:(3,5) Y0:(?,5)
-# Y1 = tf.tanh(tf.matmul(Y0, Wy) + tf.matmul(X1, Wx) + b) # Y0:(?,5), Wy:(5,5) X1:(?,3) Wx:(3,5)
 basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
-output_seqs, states = tf.contrib.rnn.static_rnn(
-                          basic_cell, [X0, X1], dtype=tf.float32)
-Y0, Y1 = output_seqs
+output_seqs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)
+outputs = tf.transpose(tf.stack(output_seqs), perm=[1, 0, 2])
 
 init = tf.global_variables_initializer()
 
-import numpy as np
-
-# Mini-batch:        instance 0,instance 1,instance 2,instance 3
-X0_batch = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 0, 1]])  # t = 0
-X1_batch = np.array([[9, 8, 7], [0, 0, 0], [6, 5, 4], [3, 2, 1]])  # t = 1
+# Mini-batch
+X_batch = np.array([
+    # t = 0     t = 1
+    [[0, 1, 2], [9, 8, 7]],  # instance 0
+    [[3, 4, 5], [0, 0, 0]],  # instance 1
+    [[6, 7, 8], [6, 5, 4]],  # instance 2
+    [[9, 0, 1], [3, 2, 1]],  # instance 3
+])
 
 with tf.Session() as sess:
     init.run()
-    Y0_val, Y1_val = sess.run([Y0, Y1], feed_dict={X0: X0_batch, X1: X1_batch})
+    outputs_val = outputs.eval(feed_dict={X: X_batch})
 
-    print(Y0_val)
-    print(Y1_val)
+    print(outputs_val)
