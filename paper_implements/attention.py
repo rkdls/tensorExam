41dac30964057da32bad6c88b405d57de403bf98
rkdls <rkdls9@naver.com>
2017-07-01 17:48:29 +0900

    mor training

--- paper_implements/attention.py
+++ paper_implements/attention.py
@@ -8,6 +8,10 @@ import numpy as np
 from collections import deque
 
 
+from attentioncell import AttentionCell, attention_masks
+from batchmake import Batcher
+
+
 def cross_entropy(labels, predict, batch_size, vocab_size):
     indices = labels + (tf.range(batch_size) * vocab_size)
     predict_flat = tf.reshape(predict, [-1])
@@ -34,154 +38,6 @@ def cross_entropy_from_indices(labels, indices, probabilities, batch_size, size)
     selection = tf.where(tf.equal(indices, targets), probabilities, tf.zeros([batch_size, size]))
     ce = -tf.log(tf.reduce_sum(selection, 1) + 1e-10)
     return ce
-
-
-class AttentionCell(tf.nn.rnn_cell.RNNCell):
-    def __init__(self, cell, attn_length, size, num_attns, lambda_type, min_tensor):
-        if not isinstance(cell, tf.nn.rnn_cell.RNNCell):
-            raise TypeError('The parameter cell is not an RNNCell.')
-        if attn_length <= 0:
-            raise ValueError('attn_length should be greater than zero, got %s'
-                             % str(attn_length))
-
-        self._cell = cell
-        self._attn_length = attn_length
-        self._size = size
-        self._num_tasks = num_attns + 1
-        self._num_attns = num_attns
-        self.lambda_type = lambda_type
-        self.min_tensor = min_tensor
-
-        print("Constructing Attention Cell")
-
-    @property
-    def state_size(self):
-        attn_sizes = [self._size * self._attn_length] * self._num_attns
-        alpha_sizes = [self._attn_length] * self._num_attns
-        attn_id_sizes = [self._attn_length] * self._num_attns
-
-        size = (self._cell.state_size,  # Language model state
-                tuple(attn_sizes),  # Attention "memory"
-                tuple(attn_id_sizes),  # Vocab ids for attention tokens
-                tuple(alpha_sizes),  # Alpha vectors (distribution over attention memory)
-                tuple([1] * self._num_attns),  # Attention Count for each attention mechanism
-                self._num_tasks)  # Number of tasks for lambda
-
-        return size
-
-    @property
-    def output_size(self):
-        return self._cell.output_size
-
-    def __call__(self, inputs, state, scope=None):
-        '''
-        Assuming that inputs is a concatenation of the embedding (batch x size)
-        and the masks (batch x num_masks)
-        :param inputs: Concatenation of input embedding and boolean mask (batch x (size+num_masks))
-        :param state:
-        :param scope:
-        :return:
-        '''
-        with tf.variable_scope(scope or type(self).__name__):
-            attn_states = state[1]
-            attn_ids = state[2]
-            attn_counts = state[4]
-            assert len(attn_states) == self._num_tasks - 1
-            # if len(attn_states) != self._num_tasks - 1:
-            #     raise ValueError("Expected %d attention states but got %d" % (self._num_tasks - 1, len(attn_states)))
-
-            state = state[0]
-            lm_input = inputs[:, 0:self._size]
-            masks = [tf.squeeze(tf.cast(inputs[:, self._size + i:self._size + i + 1], tf.bool), [1])
-                     for i in range(self._num_attns)]
-            raw_input = inputs[:, self._size + self._num_attns]
-
-            attn_inputs = [tf.reshape(attn_state, [-1, self._attn_length, self._size])
-                           for attn_state in attn_states]
-
-            lm_output, new_state = self._cell(lm_input, state)
-            h = self._state(lm_output, new_state)
-            attn_outputs_alphas = [self._attention(h, attn_inputs[i], attn_counts[i], "Attention" + str(i))
-                                   for i in range(self._num_attns)]
-            attn_outputs = [output for output, _ in attn_outputs_alphas]
-            attn_alphas = [alpha for _, alpha in attn_outputs_alphas]
-
-            lmda = self._lambda(h, attn_outputs, lm_input)
-
-            outputs = [self._lm_output(lm_output)]
-            outputs.extend(attn_outputs)
-
-            # final_output = self._weighted_output(lm_output, attn_outputs, lmda)
-            final_output = lm_output
-            new_attn_states = \
-                [self._attention_states(attn_inputs[i],
-                                        attn_ids[i],
-                                        attn_counts[i],
-                                        self._attn_input(lm_input, lm_output, final_output),
-                                        masks[i],
-                                        raw_input)
-                 for i in range(self._num_attns)]
-            # Change lm_input above for the different attention methods (eg to a slice of lm_output)
-
-            states = (new_state,
-                      tuple([tf.reshape(ns[0], [-1, self._attn_length * self._size]) for ns in new_attn_states]),
-                      tuple(ns[1] for ns in new_attn_states),
-                      tuple(attn_alphas),
-                      tuple(ns[2] for ns in new_attn_states),
-                      lmda)
-
-            attn_ids = [att[1] for att in new_attn_states]
-            return final_output, tf.transpose(tf.stack(attn_alphas), [1, 0, 2]), \
-                   tf.transpose(tf.stack(attn_ids), [1, 0, 2]), lmda, states
-
-    def _state(self, lm_output, state):
-        fully_connected = tf.contrib.layers.fully_connected(tf.concat(axis=1, values=state[-1]), self._size)
-        return fully_connected
-
-    def _lm_output(self, lm_output):
-        return lm_output
-
-    def _attn_input(self, lm_input, lm_output, final_output):
-        return lm_input
-
-    def _lambda(self, state, att_outputs, lm_input, num_tasks=None):
-        num_tasks = num_tasks or self._num_tasks
-        with tf.variable_scope("Lambda"):
-            return tf.ones([tf.shape(state)[0], num_tasks])
-
-    def _attention_states(self, attn_input, attn_ids, attn_count, lm_input, mask, raw_inputs):
-        new_attn_input = tf.concat(axis=1, values=[
-            tf.slice(attn_input, [0, 1, 0], [-1, -1, -1]),
-            tf.expand_dims(lm_input, 1)
-        ])
-
-        new_attn_ids = tf.concat(axis=1, values=[
-            tf.slice(attn_ids, [0, 1], [-1, -1]),
-            tf.expand_dims(raw_inputs, 1)
-        ])
-
-        new_attn_input = tf.where(mask, new_attn_input, attn_input)
-        new_attn_ids = tf.where(mask, new_attn_ids, attn_ids)
-        new_attn_count = tf.where(mask, tf.minimum(attn_count + 1, self._attn_length), attn_count)
-
-        return new_attn_input, new_attn_ids, new_attn_count
-
-    def _attention(self, state, attn_input, attn_counts, scope):
-        with tf.variable_scope(scope):
-            w = tf.get_variable('w', [self._size, 1])  # (size, 1)
-
-            attn_input_shaped = tf.reshape(attn_input, [-1, self._size])  # (batch, k, size) -> (batch*k, size)
-            m1 = tf.contrib.layers.fully_connected(attn_input_shaped, self._size)  # (batch*k, size)
-            m2 = tf.contrib.layers.fully_connected(state, self._size)  # (batch, size)
-            m2 = tf.reshape(tf.tile(m2, [1, self._attn_length]), [-1, self._size])  # (batch*k, size)
-            M = tf.tanh(m1 + m2)  # (batch*k, size)
-            alpha = tf.reshape(tf.matmul(M, w), [-1, self._attn_length])  # (batch, k)
-            alpha = tf.nn.softmax(alpha)
-            alpha_shaped = tf.expand_dims(alpha, 2)  # (batch, k, 1)
-
-            attn_vec = tf.reduce_sum(attn_input * alpha_shaped, 1)
-            attn_vec = tf.contrib.layers.fully_connected(attn_vec, self._size)
-            return attn_vec, alpha
 
 
 def tile_vector(vector, number):
@@ -452,83 +308,9 @@ def extract_results(results, evals, num_evals, model):
     return results[0:num_evals], state, att_states, att_ids, alpha_states, att_counts, lambda_state
 
 
-def attention_masks(attns, masks, length):
-    lst = [np.ones([1, length])]
-    return np.transpose(np.concatenate(lst)) if lst else np.zeros([0, length])
 
 
-class Batcher:
-    def __init__(self, queue, batch_size, seq_length):
-        if batch_size <= 0:
-            raise AttributeError("batch_size must be larger than 0")
 
-        self.queue = queue
-        self.seq_length = seq_length
-        self.data_queue = deque(queue)
-        self.batch_size = batch_size
-        self.counter = 0
-        self.current_data = None
-        self.current_count = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.get_batch(self.batch_size)
-
-    def get_batch(self, batch_size):
-        if self.current_count - self.counter <= self.batch_size:
-            self.load_next_data_batch()
-
-        self.counter += batch_size
-        return self.current_data[(self.counter - self.batch_size):self.counter]
-
-    def __reset(self):
-        self.data_queue = deque(self.queue)
-        self.counter = 0
-
-    def load_next_data_batch(self):
-        if not self.data_queue:
-            self.__reset()
-            raise StopIteration
-
-        current_file = self.data_queue.popleft()
-        with open(current_file, "rb") as f:
-            self.current_data = pickle.load(f)
-        self.current_count = len(self.current_data)
-        if self.current_count == 0:
-            print("Skipping partition %s which is empty" % current_file)
-            self.load_next_data_batch()
-        else:
-            print("Loaded data partition %s with %d examples" % (current_file, self.current_count))
-
-        self.counter = 0
-        gc.collect()
-
-    def sequence_iterator(self, batch):
-        n = max([b.num_sequences for b in batch])
-        for i in range(n):
-            x_arr = np.zeros([self.seq_length, self.batch_size])
-            y_arr = np.zeros([self.seq_length, self.batch_size])
-            masks_arr = np.zeros([self.seq_length, self.batch_size, 1])
-            identifier_usages = np.zeros([self.batch_size, self.seq_length])
-            actual_lengths = np.zeros([self.batch_size])
-            for j in range(self.batch_size):
-                length = 0
-                if j < len(batch) and batch[j].num_sequences > i:
-                    length = batch[j].actual_lengths[i]
-                    x_arr[0:length, j] = np.transpose(batch[j].inputs[i][0:length])
-                    y_arr[0:length, j] = np.transpose(batch[j].targets[i][0:length])
-                    if hasattr(batch[j], "var_flags"):
-                        masks_arr[0:length, j] = np.transpose(
-                            attention_masks(1, batch[j].var_flags[i], length))
-                    else:
-                        masks_arr[0:length, j, :] = attention_masks(1, batch[j].masks[i], length)
-
-                    identifier_usages[j, 0:length] = batch[j].identifier_usage[i][0:length]
-
-                actual_lengths[j] = length
-            yield (x_arr, y_arr, masks_arr, identifier_usages, actual_lengths)
 
 
 if __name__ == '__main__':
@@ -556,16 +338,11 @@ if __name__ == '__main__':
 
     data = 'data_samples/'
     pattern = 'output.txt.part*'
-    # pattern = 'preprocess.part*'
 
     files = [y for x in os.walk(data) for y in iglob(os.path.join(x[0], pattern))]
-    # current_file = deque(files).popleft()
-    # with open(current_file, 'rb') as f:
-    #     current_data = pickle.load(f)
 
     with tf.Graph().as_default(), tf.Session() as sess:
 
-        # masks_ = tf.placeholder(tf.bool, [seq_length, batch_size, attention_num], name="masks")
         masks_ = tf.placeholder(tf.bool, [seq_length, batch_size, 1], name="masks")
         input_data_ = tf.placeholder(tf.int32, [seq_length, batch_size], name="inputs")
         targets_ = tf.placeholder(tf.float32, [seq_length, batch_size], name="targets")
@@ -584,26 +361,15 @@ if __name__ == '__main__':
                            max_attention=max_attention)
         sess.run(tf.global_variables_initializer())
 
-        # labels = tf.cast(tf.reshape(a.targets, [-1]), tf.int32)
-        # mode.predict  (batch*k , vocab)
-        # cross_entropy = cross_entropy(labels, a.predict,
-        #                               a.batch_size * a.seq_length,
-        #                               a.vocab_size)
-        # mask = tf.sign(tf.abs(a.targets))  # 0이면 0, 0이상이면 1
-        # mask = tf.cast(tf.reshape(mask, [-1]), tf.float32)
-        # cross_entropy *= mask  # Zero out entries where the target is 0 (padding)
-        # cost_op = tf.reduce_sum(cross_entropy)
 
         loss = a.loss
         train = a.optimizer.minimize(loss)
-        # train = a.train_op
         initial_state = a.initial_state  # 초기 state=zero
 
         evals = [loss, train]
         evals = get_evals(evals=evals, model=a)
         print('start')
 
-        # saver = tf.train.Saver(tf.trainable_variables())
         a.assign_lr(sess, learning_rate)
         batcher = Batcher(files, batch_size, seq_length)
 
@@ -630,13 +396,3 @@ if __name__ == '__main__':
                     total_length += sum(feed_dict[a._actual_lengths])
 
             print('perplexity ', np.exp(total_loss / total_length), 'epoch ', i)
-            # lr = learning_rate if i < start_decaying else learning_rate * lr_decay
-            # now = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M--%f")
-            # out_path = os.path.join(model_path, now + "/")
-            #
-            # tf.train.write_graph(sess.graph.as_graph_def(), out_path, 'model.pb', as_text=False)
-            # if not os.path.exists(out_path):
-            #     os.makedirs(out_path)
-            # with open(os.path.join(out_path, "config.pkl"), "wb") as f:
-            #     pickle.dump(config, f)
-            # saver.save(sess, os.path.join(out_path, "model.tf"))
